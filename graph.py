9dfff763dad3362def6809d5f4d9a35f53f308d8
petey33 <psterner@gmail.com>
2023-01-24 07:26:29 +0100

    Tilføjet matplotlib og graph.py

---
+++ graph.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    y = []
+    for v in x:
+        y.append(np.sqrt(1-np.power(v,2)))
+    return y
+
+fig = plt.figure()
+ax = plt.axes()
+ax.set_aspect('equal', adjustable='box')
+
+eksponent = 2 
+a = -1
+b = 1
+x = np.linspace(a, b, 1000)
+y = f(x)
+
+plt.plot(x, y, color='red', linestyle='-', linewidth=2)
+plt.grid(b=True, color='grey', alpha=0.1, linestyle='--', linewidth=1)
+plt.show()
